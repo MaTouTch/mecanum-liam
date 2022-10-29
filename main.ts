07cf8c54ae107ba146f833453ed345aeeb2d6606
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 18:58:07 +0200

    Mecanum Remoted V1

--- main.ts
+++ main.ts
@@ -3,9 +3,13 @@ radio.onReceivedString(function (receivedString) {
         if (_switch == 0) {
             mecanumRobot.setServo(60)
             _switch = 1
+            basic.pause(LagMove)
+            mecanumRobot.setServo(90)
         } else {
             mecanumRobot.setServo(120)
             _switch = 0
+            basic.pause(LagMove)
+            mecanumRobot.setServo(90)
         }
     } else if (receivedString == "N") {
         mecanumRobot.Motor(LR.Upper_left, MD.Forward, 100)
@@ -35,8 +39,42 @@ radio.onReceivedString(function (receivedString) {
         mecanumRobot.Motor(LR.Lower_right, MD.Forward, 100)
         basic.pause(LagMove)
         mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "NL") {
+        mecanumRobot.Motor(LR.Upper_right, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_left, MD.Forward, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "NR") {
+        mecanumRobot.Motor(LR.Upper_left, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_right, MD.Forward, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "SR") {
+        mecanumRobot.Motor(LR.Upper_right, MD.Back, 100)
+        mecanumRobot.Motor(LR.Lower_left, MD.Back, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "SL") {
+        mecanumRobot.Motor(LR.Upper_left, MD.Back, 100)
+        mecanumRobot.Motor(LR.Lower_right, MD.Back, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "A") {
+        mecanumRobot.Motor(LR.Upper_left, MD.Back, 100)
+        mecanumRobot.Motor(LR.Lower_right, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Upper_right, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_left, MD.Back, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else if (receivedString == "B") {
+        mecanumRobot.Motor(LR.Upper_left, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_right, MD.Back, 100)
+        mecanumRobot.Motor(LR.Upper_right, MD.Back, 100)
+        mecanumRobot.Motor(LR.Lower_left, MD.Forward, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
     } else {
-        basic.showIcon(IconNames.Happy)
+    	
     }
 })
 let LagMove = 0
@@ -44,7 +82,7 @@ let _switch = 0
 radio.setGroup(1)
 radio.sendString("Mecanum")
 _switch = 0
-LagMove = 200
+LagMove = 50
 mecanumRobot.setServo(60)
 basic.pause(1000)
 mecanumRobot.setServo(120)
