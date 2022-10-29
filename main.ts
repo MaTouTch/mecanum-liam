946a7200693f7dcde755a5fcc00ef7549bb37ccf
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 17:28:14 +0200

    Premier test avec bouton A

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+        if (_switch == 0) {
+            mecanumRobot.setServo(0)
+            _switch = 1
+        } else {
+            mecanumRobot.setServo(90)
+            _switch = 0
+        }
+    }
+})
+let _switch = 0
+radio.setGroup(1)
+_switch = 0
 basic.forever(function () {
 	
 })
