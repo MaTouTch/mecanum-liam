14eec45af92d3cf5359801feed97844806cbda68
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 17:42:21 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,23 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "A") {
         if (_switch == 0) {
-            mecanumRobot.setServo(0)
+            mecanumRobot.setServo(60)
+            basic.showNumber(0)
             _switch = 1
         } else {
-            mecanumRobot.setServo(90)
+            mecanumRobot.setServo(120)
+            basic.showNumber(1)
             _switch = 0
         }
     }
 })
 let _switch = 0
 radio.setGroup(1)
+radio.sendString("Mecanum")
 _switch = 0
+mecanumRobot.setServo(60)
+basic.pause(1000)
+mecanumRobot.setServo(120)
 basic.forever(function () {
 	
 })
