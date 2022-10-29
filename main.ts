ef22a21b3ba01085045b1df86ff4b92dd0da83e2
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 18:14:45 +0200

    La marche avant fonctionne

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "A") {
+    if (receivedString == "D") {
         if (_switch == 0) {
             mecanumRobot.setServo(60)
             basic.showNumber(0)
@@ -9,12 +9,23 @@ radio.onReceivedString(function (receivedString) {
             basic.showNumber(1)
             _switch = 0
         }
+    } else if (receivedString == "U") {
+        mecanumRobot.Motor(LR.Upper_left, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Upper_right, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_left, MD.Forward, 100)
+        mecanumRobot.Motor(LR.Lower_right, MD.Forward, 100)
+        basic.pause(LagMove)
+        mecanumRobot.state(MotorState.stop)
+    } else {
+        basic.showIcon(IconNames.Happy)
     }
 })
+let LagMove = 0
 let _switch = 0
 radio.setGroup(1)
 radio.sendString("Mecanum")
 _switch = 0
+LagMove = 200
 mecanumRobot.setServo(60)
 basic.pause(1000)
 mecanumRobot.setServo(120)
